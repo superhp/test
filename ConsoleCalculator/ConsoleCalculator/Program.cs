0e0e28343b59aa6b9c06cad094d97d8635f704dd
Aurimas Šimkus <saurimas@hotmail.com>
2018-01-18 16:40:16 +0200

    First

---
+++ ConsoleCalculator/ConsoleCalculator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+
+            int sum = n1 + n2;
+
+            Console.WriteLine(sum);
+
+            Console.ReadLine();
+        }
+    }
+}
